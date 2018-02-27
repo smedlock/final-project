a443e989a9289c8af78a70d5f4d6f06b2f1f8448
Raine Padilla <epadilla7@mail.greenriver.edu>
2018-02-27 16:54:17 -0500

    added styling to home & register, began db functionality

--- index.php
+++ index.php
@@ -2,14 +2,40 @@
 
 error_reporting(E_ALL);
 require_once('vendor/autoload.php');
+require_once 'model/db-functions.php';
 
+session_start();
 
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
+$dbh = connect();
+
 $f3->route('GET /', function(){
     $template = new Template();
     echo $template->render('views/home.html');
+});
+
+$f3->route('GET|POST /register', function($f3){
+    if(isset($_POST['submit']))
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+       // $confirm = $_POST['confirmPass'];
+        $bio = $_POST['bio'];
+       // $premium = isset($_POST['premium']);
+
+        // Set templating variables
+        //$f3->set('username', $username);
+        //$f3->set('bio', $bio);
+
+        //$success = addUser($username, $password, $bio);
+
+        //echo $success;
+
+    }
+   $template = new Template();
+   echo $template->render('views/register.html');
 });
 
 $f3->run();
--- model/db-functions.php
+++ model/db-functions.php
@@ -1,1 +1,47 @@
 <?php
+require '/home/epadilla/final-config.php';
+
+function connect()
+{
+    try {
+        //Instantiate a database object
+        $dbh = new PDO(DB_DSN, DB_USERNAME,
+            DB_PASSWORD);
+        //echo "Connected to database!!!";
+        return $dbh;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        return;
+    }
+}
+
+function getUser()
+{
+    global $dbh;
+    // Define query
+    // Prepare statement
+    // Bind parameters
+    // Execute statement
+    // Return results
+}
+
+function addUser($username, $password, $bio)
+{
+    global $dbh;
+    // Define query
+    $sql = "INSERT INTO snake-members(username, password, bio) VALUES (:username, :password, :bio)";
+
+    // Prepare statement
+    $statement = $dbh->prepare($sql);
+
+    // Bind parameters
+    $statement->bindParam(':username', $username, PDO::PARAM_STR);
+    $statement->bindParam(':password', $password, PDO::PARAM_STR);
+    $statement->bindParam(':bio', $bio, PDO::PARAM_STR);
+
+    // Execute statement
+    $success = $statement->execute();
+
+    // Return results
+   return $success;
+}
