c2bd28ba86d2a15365ddc18639752a517d3b89f3
Scott Medlock <smedlock@mail.greenriver.edu>
2018-02-27 15:51:33 -0500

    some cleaning in snake game code

--- model/snake-game.js
+++ model/snake-game.js
@@ -1,14 +1,22 @@
-function gameBoard(width, height, cellSize) {
+function gameBoard(widthX, heightY, cellSize, ) {
     // add an exception here later
-    if (width % cellSize != 0 || height % cellSize != 0) {
-        console.log("width or height are not divisible by the cell size");
-    }
-    this.width = width;
-    this.height = height;
+    this.widthX = widthX;
+    this.heightY = heightY;
+    this.headPosX = 0;
+    this.headPosY = 0;
+    this.snakeCells = [];
     this.cellSize = cellSize;
+    this.gameBoardElement;
+
+    this.createGameBoard = function(gameBoardId) {
+        this.gameBoardElement = document.getElementById(gameBoardId);
+        this.gameBoardElement.style.background = "black";
+        this.gameBoardElement.style.width = "400px";
+        this.gameBoardElement.style.height = "400px";
+    }
 }
 
-var board = new gameBoard(400, 400, 20);
+var board = new gameBoard(20, 20, 20);
 
 function snakeCell(x, y, snakeSize, gap, background) {
     this.x = x;
@@ -19,29 +27,24 @@ function snakeCell(x, y, snakeSize, gap, background) {
     this.element.style.background = background;
     this.element.style.position = "absolute";
 
+    this.changeXY = function(x, y) {
+        this.x = x;
+        this.y = y;
+        this.element.style.top = 1 + y * snakeSize + 'px';
+        this.element.style.left = 1 + x * snakeSize + 'px';
+    }
+
     //this.changeXY = function(x, y) {
     //    this.x = x;
     //    this.y = y;
     //}
 }
 
-//var snake1 = new snakeCell(41, 81);
-
-//var newElement1 = document.createElement("div");
-//newElement1.style.width = "18px";
-//newElement1.style.height = "18px";
-//newElement1.style.background = "white";
-//newElement1.style.position = "absolute";
-//newElement1.style.top = snake1.x + 'px';
-//newElement1.style.left = snake1.y + 'px';
-//document.getElementById("container").appendChild(newElement1);
-
-console.log("we triedf");
+console.log("we trisedss");
 
 var direction = 1; // 0 up, 1 right, 2 down, 3 left
-
-var elements = [];
 var snakeCells = [];
+
 for (i = 0; i < 6; i++) {
     snakeCells[i] = new snakeCell(5 + i, 10, 20, 1, "white");
     snakeCells[i].element.style.top = snakeCells[i].y * board.cellSize + 1 + "px";
@@ -49,21 +52,7 @@ for (i = 0; i < 6; i++) {
     document.getElementById("gameboard").appendChild(snakeCells[i].element);
 }
 
-var gameboard = document.getElementById("gameboard");
-gameboard.style.background = "black";
-gameboard.style.width = "400px";
-gameboard.style.height = "400px";
-
-for (i = 0; i < 5; i++) {
-    elements[i] = document.createElement("div");
-    elements[i].style.width = "18px";
-    elements[i].style.height = "18px";
-    elements[i].style.background = "white";
-    elements[i].style.position = "absolute";
-    elements[i].style.top = 101 + 'px';
-    elements[i].style.left = 1 + 20 * i + 'px';
-    document.getElementById("gameboard").appendChild(elements[i]);
-}
+board.createGameBoard("gameboard");
 
 document.addEventListener('keydown', function(event) {
     if (event.keyCode == 87) { // w
@@ -79,43 +68,27 @@ document.addEventListener('keydown', function(event) {
 
 function startGame() {
     //var elem = document.getElementById("animate");
-    var elem = elements[elements.length - 1];
-    var posX = 200;
-    var posY = 200;
-    var id = setInterval(frame, 250);
+    var elem = snakeCells[snakeCells.length - 1];
+    var posX = elem.x;
+    var posY = elem.y;
+    var id = setInterval(frame, 500);
     function frame() {
 
         // cases:   head is in empty space
         //          head collides with wall or itself
         //          head is on same cell as food
-        /*
-        elem = elements.shift();
-        if (direction == 0) {
-            posY -= 20;
-        } else if (direction == 1) {
-            posX += 20;
-        } else if (direction == 2) {
-            posY += 20;
-        } else {
-            posX -= 20;
-        }
-        elem.style.top = 1 + posY + 'px';
-        elem.style.left = 1 + posX + 'px';
-        elements.push(elem);
-        */
 
         snakeElem = snakeCells.shift();
         if (direction == 0) {
-            posY -= 20;
+            posY -= 1;
         } else if (direction == 1) {
-            posX += 20;
+            posX += 1;
         } else if (direction == 2) {
-            posY += 20;
+            posY += 1;
         } else {
-            posX -= 20;
+            posX -= 1;
         }
-        snakeElem.element.style.top = 1 + posY + 'px';
-        snakeElem.element.style.left = 1 + posX + 'px';
+        snakeElem.changeXY(posX, posY);
         snakeCells.push(snakeElem);
     }
 }
